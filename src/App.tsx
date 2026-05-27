11bae8aae11dd4fc32e99778b5719ca4f89dfb44
Aryan Raj <aryanraj212015@gmail.com>
2026-05-27 19:14:14 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Courses from './pages/Courses';
